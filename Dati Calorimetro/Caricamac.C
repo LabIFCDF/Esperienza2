090cf2a709cb16ad034b031a5a877e16555ebd41
LabIFCDF <LordMorte97@gmail.com>
2019-12-05 18:52:22 +0100

    Introdotto dati test per equalizzazzione dei pmt e prima bozza per calcolo effficienza.

--- Dati Calorimetro/Caricamac.C
+++ Dati Calorimetro/Caricamac.C
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <math.h>
@@ -6,7 +5,7 @@
 #include <TCanvas.h>
 #include <TGraph.h>
 void Caricamac(){
-int n=20000;
+int n=1000;
 int Pmt1[n]; 
 int Pmt2[n]; 
 int Pmt3[n];
@@ -14,23 +13,25 @@ int Pmt4[n];
 int Pmt5[n];
 int Pmt6[n];
 int i;
+int z1=0;
+int v1=0;
 int num=0;
 TH1D* h1 =  new TH1D("h1", "Pmt 1",50 ,2,450);
-TH1D* h2 =  new TH1D("h2", "Pmt 2",100 ,2,480);
+TH1D* h2 =  new TH1D("h2", "Pmt 2",50 ,2,480);
 TH1D* h3 =  new TH1D("h3", "Pmt 3",50 ,2,400);
 TH1D* h4 =  new TH1D("h4", "Pmt 4",50 ,2,600);
 TH1D* h5 =  new TH1D("h5", "Pmt 5",50,2,420 );
 TH1D* h6 =  new TH1D("h6", "Pmt 6", 100 ,2,450 );
 TCanvas* c1= new TCanvas("c1", "PMT", 2000,500);
 TCanvas* c2= new TCanvas("c2", "Pmt1vsAll",2000,500);
-TCanvas * c3= new TCanvas("c3", "Pmt2vsAll",2000,500);
- c2->Divide(2,3);
- c3->Divide(2,2);
- c1->Divide(2,3);
+TCanvas* c3= new TCanvas("c3", "Pmt2vsAll",2000,500);
+c2->Divide(2,3);
+c3->Divide(2,2);
+c1->Divide(2,3);
 
       
   ifstream infile;    
-  infile.open("DatiM.txt");// file containing numbers in 3 columns 
+  infile.open("DatiTest.txt");// file containing numbers in 3 columns 
      if(infile.fail()) // checks to see if file opended 
     { 
       cout << "error" << endl;
@@ -49,45 +50,64 @@ TCanvas * c3= new TCanvas("c3", "Pmt2vsAll",2000,500);
          ++num; // go to the next number
       } 
   infile.close();
+
+  for(int i=0; i<=n-1; i++){  //Ciclo for per riscalare da eliminare in seguito
+    Pmt1[i]=Pmt1[i]-6;
+    Pmt2[i]=Pmt2[i]-4;
+    Pmt3[i]=Pmt3[i]-5;
+    Pmt4[i]=Pmt4[i]-5;
+    Pmt5[i]=Pmt5[i]-6;
+    Pmt6[i]=Pmt6[i]-7;
+  }
+
   
-   TGraph* g1= new TGraph (n,Pmt1,Pmt2);
- c2->cd(1);
- g1->Draw("AP");
-   TGraph* g2= new TGraph (n,Pmt1,Pmt3);
- c2->cd(2);
- g2->Draw("AP");
-    TGraph* g3= new TGraph (n,Pmt1,Pmt4);
- c2->cd(3);
- g3->Draw("AP");
-    TGraph* g4= new TGraph (n,Pmt1,Pmt5);
- c2->cd(4);
- g4->Draw("AP");
-    TGraph* g5= new TGraph (n,Pmt1,Pmt6);
- c2->cd(5);
- g5->Draw("AP");
+ TGraph* g1= new TGraph (n,Pmt1,Pmt2);
+   c2->cd(1);
+   g1->SetMarkerStyle(7);
+   g1->Draw("AP");
+ TGraph* g2= new TGraph (n,Pmt1,Pmt3);
+   c2->cd(2);
+   g2->SetMarkerStyle(7);
+   g2->Draw("AP");
+ TGraph* g3= new TGraph (n,Pmt1,Pmt4);
+   c2->cd(3);
+   g3->SetMarkerStyle(7);
+   g3->Draw("AP");
+ TGraph* g4= new TGraph (n,Pmt1,Pmt5);
+   c2->cd(4);
+   g4->SetMarkerStyle(7);
+   g4->Draw("AP");
+ TGraph* g5= new TGraph (n,Pmt1,Pmt6);
+   c2->cd(5);
+   g5->SetMarkerStyle(7);
+   g5->Draw("AP");
 
  TGraph* t1= new TGraph (n,Pmt2,Pmt3);
- c3->cd(1);
- t1->Draw("AP");
+   c3->cd(1);
+   t1->SetMarkerStyle(7);
+   t1->Draw("AP");
  TGraph* t2= new TGraph (n,Pmt2,Pmt4);
- c3->cd(2);
- t2->Draw("AP");
+   c3->cd(2);
+   t2->SetMarkerStyle(7);
+   t2->Draw("AP");
  TGraph* t3= new TGraph (n,Pmt2,Pmt5);
- c3->cd(3);
- t3->Draw("AP");
+   c3->cd(3);
+   t3->SetMarkerStyle(7);
+   t3->Draw("AP");
  TGraph* t4= new TGraph (n,Pmt2,Pmt6);
- c3->cd(4);
- t4->Draw("AP");
+   c3->cd(4);
+   t4->SetMarkerStyle(7);
+   t4->Draw("AP");
 
 
  
   for(int i=0; i<=n-1; i++){
-    h1->Fill(Pmt1[i]-6);
-    h2->Fill((Pmt2[i]-4)/2.5);
-    h3->Fill((Pmt3[i]-5)/3);
-    h4->Fill((Pmt4[i]-5)/2);
-    h5->Fill((Pmt5[i]-6)/2.9);
-    h6->Fill((Pmt6[i]-7)*1.2);
+    h1->Fill(Pmt1[i]);
+    h2->Fill(Pmt2[i]);
+    h3->Fill(Pmt3[i]);
+    h4->Fill(Pmt4[i]);
+    h5->Fill(Pmt5[i]);
+    h6->Fill(Pmt6[i]);
   }
   c1->cd(1);
   h1->Draw();
@@ -102,4 +122,14 @@ TCanvas * c3= new TCanvas("c3", "Pmt2vsAll",2000,500);
   c1->cd(6);
   h6->Draw();
 
+
+  for (int i=0; i<=n-1; i++){
+    if(Pmt1[i]<=9) //fissato baseline+2 per starci larghi
+	     {z1++;}
+    else v1++;
+  }
+  printf("%d/" , z1);
+  printf("/%d", v1);
+  /*eff1= (n-Accettanza*n-v1) / (n-Accettanza*n) */
+  
 } 
